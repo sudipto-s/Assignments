19783a950d32926f771c385c282cf582fd93e653
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-04-28 20:41:02 +0530

    Update HQ5.java

--- DSA 2nd Sem/Assignment2/HQ5.java
+++ DSA 2nd Sem/Assignment2/HQ5.java
@@ -10,6 +10,7 @@ interface HotelService {
 class RoomServiceMeal implements RestaurantMeal, HotelService {
     void display() {
         System.out.println("A '"+name+"' costing Rs. "+price+" is a '"+serviceName+"' provided to room "+roomNumber+" for Rs "+fee+" fee");
+        System.out.println("The total fee to be paid is Rs. "+(price+fee));
     }
     public static void main(String[] args) {
         RoomServiceMeal a = new RoomServiceMeal();
