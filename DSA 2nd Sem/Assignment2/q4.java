68d7b7b9e3f5068af741a06200a0dea30c0014a8
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-05-01 18:34:14 +0530

    Update q4.java

--- DSA 2nd Sem/Assignment2/q4.java
+++ DSA 2nd Sem/Assignment2/q4.java
@@ -1,28 +1,26 @@
-class Product {
+class q4 {
     String pid;
     double price;
     static double tot_price;
-    Product(String pid, double price) {
+    q4(String pid, double price) {
        this.pid = pid;
        this.price = price;
     }
     void display() {
         System.out.println(pid+"\tPrice: "+price);
     }
-    static double totalAmount(Product[] pr) {
+    static double totalAmount(q4[] pr) {
         for (int i = 0; i < pr.length; i++)
             tot_price += pr[i].price;
         return tot_price;
     }
-}
-public class q4 {
     public static void main(String[] args) {
-        Product[] p =  new Product[5];
-        p[0] = new Product("Screw", 879);
-        p[1] = new Product("Bolt", 258.1);
-        p[2] = new Product("Spanner", 156);
-        p[3] = new Product("Wire", 2500.1);
-        p[4] = new Product("Pipe", 916.6);
+        q4[] p =  new q4[5];
+        p[0] = new q4("Screw", 879);
+        p[1] = new q4("Bolt", 258.1);
+        p[2] = new q4("Spanner", 156);
+        p[3] = new q4("Wire", 2500.1);
+        p[4] = new q4("Pipe", 916.6);
         for (int i = 0; i < p.length; i++)
             p[i].display();
         System.out.println(totalAmount(p));
