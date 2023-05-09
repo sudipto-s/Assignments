c1ad3703b9ef68649b2b3f392a4a4b8052c21f76
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-05-09 08:30:03 +0530

    Update Test.java

--- 2241019529/Test.java
+++ 2241019529/Test.java
@@ -1,67 +1,53 @@
 import java.util.*;
-class Date {
-    int day, month, year;
-    public Date(int day, int month, int year) {
-        this.day = day;
-        this.month =month;
-        this.year = year;
-    }
-    void getDate() {
-        
-    }
-}
-class Address extends Date {
-    String address;
-    public Address(String address) {
-        super(d, m, y);
-        this.address = address;
-    }
-    void getAddress() {
-        
-    }
-}
-class Employee extends Address {
-    String name, jobPosition, contactNumeber, address;
-    int empId;
-    double salary;
-    int day, month, year;
-    public Employee(String name, String jobPosition, String contactNumber, int empId, double salary) {
-        super(a, d, m, y);
-        this.name = name;
-        this.jobPosition = jobPosition;
-        this.contactNumeber = contactNumber;
-        this.empId = empId;
-        this.salary = salary;
-    }
-    void getEmployee() {
-        
-    }
-}
 public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Employee[] e = new Employe[2];
+        Employee[] e = new Employee[2];
         for (int i = 0; i < e.length; i++) {
-            System.out.println("Enter details");
-            e[i] = new Employee();
-            e[i].setAddress("Bhubaneshwar");
-            e[i].setDate(i+1, i+2, i+2000);
-            e[i].setData("A", "Manager", "912586974265", 1256+i, 57000+i);
+            e[i] = new Employee("Ajshb", "Manager", 1258, 58000, "584779", "L1, Bh, Od", "IN", 751030, 25, 05, 2015);
         } sc.close();
+        for (int i = 0; i < e.length; i++) {
+            e[i].getDetails();
+        }
         /*public static void arrangeEmployeeBySalary(Employe[] e) {
+            for (int i = 0; i < e.length; i++) {
+                
+            }
+        }
+        /*public static void getEmployeeByJobPosition(Employe[] e, String jp) {
             
         }
-        public static void getEmployeeByJobPosition(Employe[] e) {
+        /*public static void getEmployeeByHireDate(Employe[] e, Date d1, Date d2) {
             
         }
-        public static void getEmployeeByHireDate(Employe[] e) {
+        /*public static void foreignEmployeeCount(Employe[] e) {
             
         }
-        public static void foreignEmployeeCount(Employe[] e) {
-            
-        }
-        public static void getEmployeeBySalary(Employe[] e) {
+        /*public static void getEmployeeBySalary(Employe[] e, double s1, double s2) {
             
         }*/
     }
 }
+
+
+
+
+
+
+
+// To populate Employee
+/*System.out.println("Enter details");
+            String name = sc.next();
+            String jobP = sc.next();
+            String cNum = sc.next();
+            int eId = sc.nextInt();
+            double salry = sc.nextDouble();
+            String stre = sc.next();
+            String cit = sc.next();
+            String sta = sc.next();
+            String coun = sc.next();
+            int pin = sc.nextInt();
+            int d = sc.nextInt();
+            int m = sc.nextInt();
+            int y = sc.nextInt();
+            e[i] = new Employe(name, jobP, cNum, eId, salry, stre, cit, sta, coun, pin, d, m, y); */
