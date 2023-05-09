f4acf38d0d175a548094603ff7cb9b4e26971336
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-05-09 20:17:14 +0530

    Update Test.java

--- 2241019529/Test.java
+++ 2241019529/Test.java
@@ -2,30 +2,47 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Employee[] e = new Employee[2];
+        Employe[] e = new Employe[2];
         for (int i = 0; i < e.length; i++) {
-            e[i] = new Employee("Ajshb", "Manager", 1258, 58000, "584779", "L1, Bh, Od", "IN", 751030, 25, 05, 2015);
+            e[i] = new Employe("Sudipta", "manager", 1258, 59000+i, "911234567890", "ITER, Bhubaneshwar", "India", 851030, 5, 6, 2015);
         } sc.close();
-        for (int i = 0; i < e.length; i++) {
-            e[i].getDetails();
-        }
-        /*public static void arrangeEmployeeBySalary(Employe[] e) {
-            for (int i = 0; i < e.length; i++) {
-                
+        // for (int i = 0; i < e.length; i++)
+        //     e[i].getDetails();
+        arrangeEmployeeBySalary(e);
+        getEmployeeByJobPosition(e, "manager");
+        foreignEmployeeCount(e);
+    }
+    public static void arrangeEmployeeBySalary(Employe[] e) {
+        for (int i = 0; i < e.length; i++)
+            for (int j = 0; j < e.length; j++) {
+                if(e[i].salary > e[j].salary) {
+                    Employe temp = e[i];
+                    e[i] = e[j];
+                    e[j] = temp;
+                }
             }
+    }
+    public static void getEmployeeByJobPosition(Employe[] e, String jp) {
+        for (int i = 0; i < e.length; i++)
+            if (e[i].jobPosition == jp)
+                e[i].getDetails();
+    }
+    /*public static void getEmployeeByHireDate(Employe[] e, Date d1, Date d2) {
+        for (int i = 0; i < e.length; i++) {
+            int d = e[i].day, m = e[i].month, y = e[i].year;
+            if ((d >= d1.day && d <= d2.day) && (m >= d1.month && m <= d2.month) && (y >= d1.year && y <= d2.year))
+                e[i].getDetails();
         }
-        /*public static void getEmployeeByJobPosition(Employe[] e, String jp) {
-            
-        }
-        /*public static void getEmployeeByHireDate(Employe[] e, Date d1, Date d2) {
-            
-        }
-        /*public static void foreignEmployeeCount(Employe[] e) {
-            
-        }
-        /*public static void getEmployeeBySalary(Employe[] e, double s1, double s2) {
-            
-        }*/
+    } */
+    public static void foreignEmployeeCount(Employe[] e) {
+        int c = 0;
+        for (int i = 0; i < e.length; i++)
+            if ((e[i].contactNumeber.substring(0, 2) != "91"))
+                c++;
+        System.out.println("Number of Foreign employee(s): "+c);
+    }
+    public static void getEmployeeBySalary(Employe[] e, double s1, double s2) {
+        
     }
 }
 
@@ -38,7 +55,7 @@ public class Test {
 // To populate Employee
 /*System.out.println("Enter details");
             String name = sc.next();
-            String jobP = sc.next();
+            String jobP = sc.next().toLowerCase();
             String cNum = sc.next();
             int eId = sc.nextInt();
             double salry = sc.nextDouble();
