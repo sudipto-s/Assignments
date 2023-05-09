74c5c56809333ca848cad086656be9be1cc97e20
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-05-09 18:08:16 +0530

    Update Employee.java

--- 2241019529/Employee.java
+++ 2241019529/Employee.java
@@ -1,7 +1,7 @@
 
 class Date {
     int day, month, year;
-    public Date(int day, int month, int year) {
+    Date(int day, int month, int year) {
         this.day = day;
         this.month =month;
         this.year = year;
