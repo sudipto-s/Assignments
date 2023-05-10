5e20abaaa3400078867a41e5ca8efd4ad336e800
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-05-10 17:15:04 +0530

    Add files via upload

---
+++ DSA 2nd Sem/Assignment3/Q1.java
@@ -0,0 +1,18 @@
+package DSA_2nd_Sem.Assignment3;
+
+import java.util.*;
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your lucky numebr: ");
+        int n = sc.nextInt();
+        sc.close();
+        try {
+            if (n < 0)
+                throw new NumberFormatException("Negative number");
+            System.out.println("Your lucky number is: "+n);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
