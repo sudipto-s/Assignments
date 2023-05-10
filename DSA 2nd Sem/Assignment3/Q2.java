a9194bb23c0d069f8bb308a3bf228b9409845411
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-05-10 17:15:43 +0530

    Update Q2.java

--- DSA 2nd Sem/Assignment3/Q2.java
+++ DSA 2nd Sem/Assignment3/Q2.java
@@ -1,7 +1,4 @@
-package DSA_2nd_Sem.Assignment3;
-
-import java.util.Scanner;
-
+import java.util.*;
 public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
