76eaf466f2922dc6fecffb8b3be790b730795758
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-06-21 16:44:29 +0530

    Update LinkedList.java

--- DSA/Linkedlist/LinkedList.java
+++ DSA/Linkedlist/LinkedList.java
@@ -1,29 +1,3 @@
-// package Linkedlist;
-
-// public class LinkedList {
-    
-//     private Node head;
-//     private Node tail;
-//     private int length;
-
-//     class Node {
-//         int value;
-//         Node next;
-
-//         Node(int value) {
-//             this.value = value;
-//         }
-//     }
-
-//     public LinkedList(int value) {
-//         Node newNode = new Node(value);
-//         head = newNode;
-//         tail = newNode;
-//         length = 1;
-//     }
-// }
-
-
 public class LinkedList {
 
 // CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE //
