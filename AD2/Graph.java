d709d976ac9e9f4a942e0f41ea6c9cc9583a6856
Sudipta Singha <singhaashoka03@gmail.com>
2024-03-08 07:37:25 +0530

    Update AD2 Lab 2 solution

--- AD2/Graph.java
+++ AD2/Graph.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+/* Q1 - Adjacency list representation of graph */
 public class Graph {
     int count;
     private LinkedList<LinkedList<Edge>> Adj;
@@ -37,24 +38,6 @@ public class Graph {
         addUndirectedEdge(source, dest, 1);
     }
 
-    static boolean dfsStack(Graph gph, int source, int target) {
-        int count = gph.count;
-        boolean[] visited = new boolean[count];
-        Stack<Integer> stk = new Stack<Integer>();
-        stk.push(source);
-        visited[source] = true;
-        while (!stk.isEmpty()) {
-            int curr = stk.pop();
-            LinkedList<Edge> adl = gph.Adj.get(curr);
-            for (Edge adn : adl)
-                if (!visited[adn.dest]) {
-                    visited[adn.dest] = true;
-                    stk.push(adn.dest);
-                }
-        }
-        return visited[target];
-    }
-
     void print() {
         for (int i = 0; i < count; i++) {
             LinkedList<Edge> ad = Adj.get(i);
@@ -73,10 +56,109 @@ public class Graph {
         grp.addDirectedEdge(2, 3, 1);
         grp.addDirectedEdge(3, 2, 1);
         grp.print();
-        System.out.println(dfsStack(grp, 1, 2));
     }
 }
 
-// WAP to represent a graph in a adjacency list repesentation.
-// Stack based implementation of DFS.
-// HW - Queue based BFS, Example 12.8, 12.22
+/* Q2 - DFS */
+/* public static boolean dfsStack(Graph gph, int source, int target) {
+    int count = gph.count;
+    boolean[] visited = new boolean[count];
+    Stack<Integer> stk = new Stack<Integer>();
+    stk.push(source);
+    visited[source] = true;
+    while (!stk.isEmpty()) {
+        int curr = stk.pop();
+        LinkedList<Edge> adl = gph.Adj.get(curr);
+        for (Edge adn : adl)
+            if (visited[adn.dest] == false) {
+                visited[adn.dest] = true;
+                stk.push(adn.dest);
+            }
+    }
+    return visited[target];
+} */
+
+
+/* Q3 - BFS */
+/* public static boolean bfs(Graph gph, int source, int target) {
+    int count = gph.count;
+    boolean[] visited = new boolean[count];
+    LinkedList<Integer> que = new LinkedList<Integer>();
+    que.add(source);
+    visited[source] = true;
+    while (!que.isEmpty()) {
+        int curr = que.remove();
+        LinkedList<Edge> adl = gph.Adj.get(curr);
+        for (Edge adn : adl) {
+            if (visited[adn.dest] == false) {
+                visited[adn.dest] = true;
+                que.add(adn.dest);
+            }
+        }
+    }
+    return visited[target];
+} */
+
+
+/* Q4 - Example 12.8 Coount all path DFS */
+/* public static int countAllpathDFS(Graph gph, boolean[] visited, int source, int dest) {
+    if(source == dest)
+        return 1;
+    int count = 0;
+    visited[source] = true;
+    LinkedList<Edge> adl = gph.Adj.get(source);
+    for (Edge adn : adl) {
+        if(visited[adn.dest] == false)
+            count += countAllpathDFS(gph, visited, adn.dest, dest);
+        visited[source] = false;
+    }
+    return count;
+}
+public static int countAllPaths(Graph gph, int source, int dest) {
+    int count = gph.count;
+    boolean[] visited = new boolean[count];
+    return countAllpathDFS(gph, visited, source, dest);
+} */
+
+
+/* Q5 - Example 12.22 Prim's Algorithm (MST) */
+/* public static int prims(Graph gph) {
+    int count = gph.count;
+    int[] prev = new int[count];
+    int[] dist = new int[count];
+    boolean[] visited = new boolean[count];
+    int src = 1;
+    for (int i = 0; i < count; i++) {
+        prev[i] = -1;
+        dist[i] = 999999;
+    }
+    dist[src] = 0;
+    prev[src] = -1;
+    EdgeComparator comp = new EdgeComparator();
+    PriorityQueue<Edge> que = new PriorityQueue<Edge>(100, comp);
+    Edge node = new Edge(src, 0);
+    que.add(node);
+    while (!que.isEmpty()) {
+        node = que.peek();
+        que.remove();
+        visited[src] = true;
+        src = node.dest;
+        LinkedList<Edge> adl = gph.Adj.get(src);
+        for (Edge adn : adl) {
+            int dest = adn.dest;
+            int alt = adn.cost;
+            if (dist[dest] > alt && visited[dest] == false) {
+                dist[dest] = alt;
+                prev[dest] = src;
+                node = new Edge(dest, alt);
+                que.add(node);
+            }
+        }
+    }
+    for (int i = 0; i < count; i++) {
+        if (dist[i] == Integer.MAX_VALUE)
+            System.out.println("node id "+i+" prev "+prev[i]+" distance: Unreachable");
+        else
+            System.out.println("node id "+i+" prev "+prev[i]+" distance: "+dist[i]);
+    }
+} */
