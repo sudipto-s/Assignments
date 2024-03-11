582b41a3f9cff9e779150195f81e0030fea07498
Sudipta Singha <singhaashoka03@gmail.com>
2024-03-11 18:36:54 +0530

    Update MWDW2

---
+++ MWDW2/Questions/sol.js
@@ -0,0 +1,238 @@
+/* Q3 */
+// let perpenicular = prompt('Enter value of perpendicular'),
+// base = prompt('Enter value of base')
+// let hypotenuse = (perpenicular ** 2 + base ** 2) ** 0.5
+// console.log(hypotenuse)
+
+/* Q4 */
+// var a = 1, b = 2, c = 3
+// a += b
+// a /= c
+// c %= b
+//console.log(a, b, c)
+
+/* Q5 */
+// var miles = 6,
+// km = miles * 1.60935
+//console.log(km)
+
+/* Q6 */
+// let weight = prompt('Enter weight in pound'),
+// height = prompt('Enter height in inch')
+// weight *= 0.454
+// height *= 0.0254
+// let bmi = weight / (height * height)
+// console.log(bmi)
+
+/* Q7 */
+// var ar1 = ['Milk', 'Bread', 'Apple']
+//console.log(ar1.length)
+// ar1[1] = 'Banana'
+//console.log(ar1)
+
+/* Q8 */
+// var ar2 = []
+// ar2.push('Milk', 'Bread', 'Apple')
+// ar2.splice(1, 1, 'Banana', 'Egg')
+// console.log(ar2.pop())
+// ar2.sort()
+// console.log(ar2.indexOf('Milk'))
+// ar2.splice(1, 0, 'Carrot', 'Lettuce')
+// var ar3 = ['Juice', 'Pop']
+// ar2 = ar2.concat(ar3, ar3)
+// console.log(ar2.lastIndexOf('Pop'))
+// console.log(ar2)
+
+/* Q9 */
+// var ar1 = [1, 2, 3], ar2 = []
+// ar2 = ar2.concat(ar1, ar1, ar1)
+// let num = ar2.find(e => e === 2)
+//console.log(num)
+
+/* Q10 */
+// let ob1 = {
+//     model: "Jesko",
+//     make: "Koeigsegg",
+//     color: "Silver",
+//     class: "Sports car",
+//     engineCC: 5056,
+//     varients: ["Attack", "Absolut"],
+//     year: 2021,
+//     assembly: "Angelholm, Sweden"
+// }
+// var prop = "color"
+// ob1[prop] = "Grey"
+// prop = "forSale"
+// ob1[prop] = true
+// console.log("Model: ", ob1.model)
+// console.log("Make: ", ob1.make)
+// console.log("For sale? ", ob1.forSale)
+
+/* Q11 */
+// var people = {
+//         friends: []
+//     },
+//     f1 = {
+//         fName: 'name 1',
+//         lName: 'last name 1',
+//         id: 1
+//     },
+//     f2 = {
+//         fName: 'name 2',
+//         lName: 'last name 2',
+//         id: 2
+//     },
+//     f3 = {
+//         fName: 'name 3',
+//         lName: 'last name 3',
+//         id: 3
+//     }
+// people.friends.push(f1, f2, f3)
+// console.log(people)
+
+/* Q12 */
+// let q12 = ['Laurence', 'Svekis', true, 35, null, undefined,
+//     {
+//         test: 'one',
+//         score: 55
+//     },
+//     ['one', 'two']
+// ]
+// q12.shift()
+// q12.pop()
+// q12.unshift('FIRST')
+// q12.pop()
+// q12.pop()
+// q12.pop()
+// q12.pop()
+// q12.pop()
+// q12.push('MIDDLE', 'hello world', 'LAST')
+// console.log(q12)
+
+/* Q13 */
+// let inv = []
+// var p1 = {
+//         name: '',
+//         model: '',
+//         cost: 1,
+//         quantity: 1
+//     },
+//     p2 = {
+//         name: '',
+//         model: '',
+//         cost: 1,
+//         quantity: 1
+//     },
+//     p3 = {
+//         name: '',
+//         model: '',
+//         cost: 1,
+//         quantity: 1
+//     }
+// inv.push(p1, p2, p3)
+// console.log(inv)
+
+/* Q14 */
+// let term = true
+// console.log(term)
+// if(term)
+//     console.log(term)
+// if(!term)
+//     console.log(!term)
+
+/* Q15 */
+// let age = prompt('Enter your age')
+// age = Number(age)
+// var msg
+// if(age >= 21)
+//     msg = "entry to venue"
+
+/* Q16 */
+// var id = true,
+// msg = id ? "valid" : "invalid"
+// console.log(msg)
+
+/* Q17 incomplete*/
+// switch (key) {
+//     case 0:
+        
+//         break;
+//     case 1:
+        
+//         break;
+//     case 2:
+        
+//         break;
+//     case 3:
+        
+//         break;
+//     case 4:
+        
+//         break;
+//     case 5:
+        
+//         break;
+//     default:
+// }
+
+/* Q18 */
+// var prize = prompt("Enter a number between 0 & 10")
+// prize = +prize
+// var msg = "My selection: "
+// switch (prize) {
+//     case 1:
+//     case 2:
+//     case 3:
+//         msg += prize
+//         break;
+//     case 4:
+//     case 5:
+//     case 6:
+//         msg += prize
+//         break;
+//     case 7:
+//     case 8:
+//     case 9:
+//     case 10:
+//         msg += prize
+//         break;
+//     default:
+//         msg += " wrong selection!"
+// }
+// console.log(msg)
+
+/* Q19 */
+// var friend = prompt("Enter a name"), msg
+// switch (friend) {
+//     case "Debi":
+//     case "Aaditya":
+//     case "Ashutosh":
+//     case "Pushkar":
+//         msg = "Friend confirmed"
+//         break;
+//     default:
+//         msg = "Friend not confirmed"
+// }
+// console.log(msg)
+
+/* Q20 incomplete*/
+// var hk = ['rock', 'paper', 'scissor'],
+// comp = hk[Math.floor(Math.random()*3)]
+// user = prompt('Enter your choice: 0 - Rock, 1 - Paper or 2 - Scissor')
+
+// console.log(comp)
+
+/* Q21 */
+
+
+/* Q22 */
+
+/* Q23 */
+
+/* Q24 */
+
+/* Q25 */
+
+/* Q26 */
+
+/* Q27 */
