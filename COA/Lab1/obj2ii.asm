9442dad27c7528af78327019f57b16595eb3dd12
Sudipta Singha <singhaashoka03@gmail.com>
2024-03-13 19:30:50 +0530

    Update COA Lab 1 files

---
+++ COA/Lab1/obj2ii.asm
@@ -0,0 +1,17 @@
+; NAME: SUDIPTA SINGHA
+; REDG: 2241019529
+
+; ii. Y = (data1 and data2 ) or (data1 xor data2)
+MOV [1000H], 12H
+MOV [1001H], 24H
+MOV SI, 1000H
+MOV AL, [SI]
+MOV CL, AL
+INC SI
+MOV BL, [SI]
+AND AL, BL
+XOR CL, BL
+OR AL, CL
+INC SI
+MOV [SI], AL
+HLT
---
+++ COA/Lab1/obj4.asm
@@ -0,0 +1,10 @@
+; NAME: SUDIPTA SINGHA
+; REDG: 2241019529
+
+; 8 bit 2's compliment
+mov al, 34h
+not al
+inc al
+mov [5000h], al
+
+hlt
