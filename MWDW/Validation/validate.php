c94e4f888b010294dc8311b78df9713c977bd64e
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2024-03-15 10:54:42 +0530

    Update validate.php

--- MWDW/Validation/validate.php
+++ MWDW/Validation/validate.php
@@ -9,17 +9,18 @@ echo $username;
 // if (empty($name) || empty($username) || empty($email) || empty($password))
 //     echo "All fields are required!";
 // else {
-    // Username
-    if (preg_match("/^[a-zA-Z]\s\*[0-9._]+$/", $username))
+    /* Username */
+    if (preg_match_all("/^[a-zA-Z0-9._]+$/", $username))
         echo "Invalid username!";
 
-    // Email
+    /* Email */
+    // if (preg_match_all("/([a-zA-Z0-9._-]+\@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/", $email))
     // if (!filter_var($email, FILTER_VALIDATE_EMAIL))
     //     echo "Invalid email!";
 
-    // // Password 
+    /* Password */
     // if (strlen($password) >= 5) {
-    //     if (!preg_match("/[a-zA-Z0-9._]+/", $password))
+    //     if (!preg_match_all("/^[a-zA-Z0-9.@#]{5,}$/", $password))
     //         echo "Invalid password!";
     // } else
     //     echo "Password length should be greater than 4";
