c024fb9978f99e5495537624c998e50ff4418ae7
Sudipta Singha <singhaashoka03@gmail.com>
2024-03-16 07:49:11 +0530

    Update validation(incomplete)

--- MWDW/Validation/validate.php
+++ MWDW/Validation/validate.php
@@ -6,23 +6,23 @@ $password = $_POST['password'];
 
 echo $username;
 
-// if (empty($name) || empty($username) || empty($email) || empty($password))
-//     echo "All fields are required!";
-// else {
+echo "PHP validation!";
+if (empty($name) || empty($username) || empty($email) || empty($password))
+    echo "All fields are required!";
+else {
     /* Username */
-    if (preg_match_all("/^[a-zA-Z0-9._]+$/", $username))
+    if (!preg_match_all("/^[a-zA-Z0-9._]+$/", $username))
         echo "Invalid username!";
 
     /* Email */
-    // if (preg_match_all("/([a-zA-Z0-9._-]+\@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/", $email))
-    // if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-    //     echo "Invalid email!";
+    if (!preg_match_all("/([a-zA-Z0-9._-]+\@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/", $email))
+        echo "Invalid email!";
 
     /* Password */
-    // if (strlen($password) >= 5) {
-    //     if (!preg_match_all("/^[a-zA-Z0-9.@#]{5,}$/", $password))
-    //         echo "Invalid password!";
-    // } else
-    //     echo "Password length should be greater than 4";
-// }
+    if (strlen($password) >= 5) {
+        if (!preg_match_all("/^[a-zA-Z0-9.@#]{5,}$/", $password))
+            echo "Invalid password!";
+    } else
+        echo "Password length should be greater than 4";
+}
 
