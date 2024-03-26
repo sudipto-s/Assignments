7507077d24ccb019522638197729f39baed392a4
Sudipta Singha <singhaashoka03@gmail.com>
2024-03-26 09:37:13 +0530

    Update COA Lab 2 postlab2.asm

--- COA/Lab2/postlab2.asm
+++ COA/Lab2/postlab2.asm
@@ -1,7 +1,7 @@
 ; Post lab - Q2 - Divide two 16-bit numbers without DIV instruction in direct addressing mode
 
-MOV AX, 0064H
-MOV BX, 04H
+MOV AX, [1000H]  ; 0064H
+MOV BX, [1002H]  ; 04H
 XOR CX, CX
 
 L1:
