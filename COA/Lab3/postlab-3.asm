02a95d08d82c01d8400008b305fca420efa83ff3
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2024-04-13 11:40:30 +0530

    Update postlab-3.asm

--- COA/Lab3/postlab-3.asm
+++ COA/Lab3/postlab-3.asm
@@ -2,7 +2,7 @@
 
 .DATA
     SIZE DB 05H                     ; Length of the array
-    ARR DB 01H, 02H, 03H, 04H, 05H  ; Example array of numbers
+    ARR DB 01H, 02H, 03H, 04H, 05H  ; Example array of 8-bit numbers
 
     SUM DB 0                        ; Variable to store the sum
     AVG DB 0                        ; Variable to store the average
@@ -10,19 +10,19 @@
 .CODE
 MAIN PROC
     MOV AX, DATA
-    MOV DS, AX
-                                    ; Load size to CL
-    MOV CL, SIZE                    ; Load array
-    LEA SI, ARR                     ; Clear AX
-    XOR AX, AX
+    MOV DS, AX                      ; Load data into SI - 0710:0000
+                                    
+    MOV CL, SIZE                    ; Load size into CL
+    LEA SI, ARR                     ; Load array - 0701:0001, :0002....
+    XOR AX, AX                      ; Clear AX
     L1:
         ADD AL, [SI]                ; Add AL & [SI]
         INC SI
         DEC CL
         JNZ L1
-    MOV SUM, AL                     ; Load SUM
+    MOV SUM, AL                     ; Load Al to the variable SUMA
     MOV BL, SIZE                    ; Load size to BL
-    DIV BL                          ; Div AL/BL
+    DIV BL                          ; AX/BL
     MOV AVG, AL                     ; Load average to AVG
     
 END MAIN
