622947171fee55f4d70b9d9e0a361c049fdcb755
Sudipta Singha <singhaashoka03@gmail.com>
2024-06-08 20:31:16 +0530

    Update AD2 Lab 7

---
+++ AD2/Lab7/knapSack01.java
@@ -0,0 +1,26 @@
+/* Lab 7 */
+
+/* 0/1 Knapsack problem */
+
+public class knapSack01 {
+    static int getMaxValue(int[] weight, int[] value, int capacity, int n) {
+        if (n == 0 || capacity == 0)
+            return 0;
+        if (weight[n - 1] > capacity)
+            return getMaxValue(weight, value, capacity, n - 1);
+        else
+            return max(value[n - 1] + getMaxValue(weight, value, capacity - weight[n - 1], n - 1), getMaxValue(weight, value, capacity, n - 1));
+    }
+
+    static int max(int x, int y) {
+        return x > y ? x : y;
+    }
+    
+    public static void main(String[] args) {
+        int capacity = 4;
+        int[] weight = { 2, 2, 2 };
+        int[] value = { 300, 190, 180};
+        int maxValue = getMaxValue(weight, value, capacity, weight.length);
+        System.out.println(maxValue);
+    }
+}
