a22254601c772e6c293d4dfe9f08979921f4865e
Sudipta Singha <singhaashoka03@gmail.com>
2024-10-16 09:20:51 +0530

    Update C A2 q3

--- 5th sem/C/A2/q3.c
+++ 5th sem/C/A2/q3.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-void display(int, double, double, double);
+void display(int odometer, double emit, double max1, double max2) {
+   if (odometer <= 50000) {
+      if (emit <= max1)
+         printf("Emissions are under permitted level of %lf grams/mile.", max1);
+      else
+         printf("Emissions exceed permitted level of %lf grams/mile.", max1);
+   } else {
+      double myEmit = odometer * emit;
+      double left = odometer - 50000;
+      double limit = (50000 * max1) + (left * max2);
+      if (myEmit <= limit)
+         printf("Emissions are under permitted level of %lf grams/mile.", max2);
+      else
+         printf("Emissions exceed permitted level of %lf grams/mile.", max2);
+   }
+}
 
 void main() {
    int plt_num, odometer;
@@ -14,8 +29,16 @@ void main() {
    }
    printf("Enter number of grams emitted per mile: ");
    scanf("%lf", &emit);
+   if (emit < 0) {
+      printf("Invalid input");
+      return;
+   }
    printf("Enter odometer reading: ");
    scanf("%d", &odometer);
+   if (odometer < 0) {
+      printf("Invalid input");
+      return;
+   }
    
    switch (plt_num) {
       case 1:
@@ -32,18 +55,4 @@ void main() {
          break;
    }
    return;
-}
-
-void display(int odometer, double emit, double max1, double max2) {
-   if (odometer <= 50000) {
-      if (emit <= max1)
-         printf("Emissions are under permitted level of %lf grams/mile.", max1);
-      else
-         printf("Emissions exceed permitted level of %lf grams/mile.", max1);
-   } else {
-      if (emit <= max2)
-         printf("Emissions are under permitted level of %lf grams/mile.", max2);
-      else
-         printf("Emissions exceed permitted level of %lf grams/mile.", max2);
-   }
 }
