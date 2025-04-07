ce5fc214d238c4641136cac892387879f974a6d6
Sudipta Singha <115441575+sudipto-s@users.noreply.github.com>
2025-04-07 12:41:26 +0530

    Update second.js

--- 6th sem/SSWD/A1/q4/second.js
+++ 6th sem/SSWD/A1/q4/second.js
@@ -1,4 +1,4 @@
-const { area, circumference } = require("./circle.js")
+const { myarea, mycircumference } = require("./circle.js")
 
-console.log(`Area: ${area(5)}`)
-console.log(`Circumference: ${circumference(5)}`)
+console.log(`Area: ${myarea(5)}`)
+console.log(`Circumference: ${mycircumference(5)}`)
