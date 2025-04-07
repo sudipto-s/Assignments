1fb4d4f695e2cdec547d1ccff7616a3857d1c142
Sudipta Singha <115441575+sudipto-s@users.noreply.github.com>
2025-04-07 12:39:21 +0530

    Update and rename second.mjs to second.js

---
+++ 6th sem/SSWD/A1/q4/second.js
@@ -0,0 +1,4 @@
+const { area, circumference } = require("./circle.js")
+
+console.log(`Area: ${area(5)}`)
+console.log(`Circumference: ${circumference(5)}`)
--- 6th sem/SSWD/A1/q4/second.mjs
+++
@@ -1,4 +0,0 @@
-import { area, circumference } from "./circle.mjs"
-
-console.log(`Area: ${area(5)}`)
-console.log(`Circumference: ${circumference(5)}`)
